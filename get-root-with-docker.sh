4f31e3c6b34e6590a85671a586bbabeb1421d5da
Schspa Shi <schspa@gmail.com>
2023-06-06 15:49:45 +0800

    root: use exec to avoid parent exit
    Signed-off-by: Schspa Shi <schspa@gmail.com>

--- get-root-with-docker.sh
+++ get-root-with-docker.sh
@@ -13,12 +13,12 @@ int main(int argc, char *argv[])
 
     printf("real uid\t %d\n",getuid());
     printf("effective uid\t %d\n",geteuid());
-    if(setuid(0) == -1)
+    if (setuid(0) == -1)
     {
         perror("setuid(0) failed");
         return -1;
     }
-    if(setgid(0) == -1)
+    if (setgid(0) == -1)
     {
         perror("setgid(0) failed");
         return -1;
@@ -31,4 +31,4 @@ ENDOFMESSAGE
 docker run --rm -v $(pwd):/work ubuntu chown root:root /work/enter_root_bash
 docker run --rm -v $(pwd):/work ubuntu chmod u+s /work/enter_root_bash
 
-./enter_root_bash
+exec ./enter_root_bash
