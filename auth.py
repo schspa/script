b1150d098bd6c2f50f8d646bc30621bed2f97533
Schspa Shi <schspa@gmail.com>
2023-09-25 17:18:52 +0800

    script: add user_access_token from feishu
    Signed-off-by: Schspa Shi <schspa@gmail.com>

---
+++ auth.py
@@ -0,0 +1,298 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#   auth.py --- Description
+#
+#   Copyright (C) 2023, Schspa, all rights reserved.
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+import os
+import requests
+import logging
+import json
+import urllib.parse
+import time
+from tenacity import retry, stop_after_attempt, wait_random
+from loguru import logger
+import keyring
+import http.server
+from urllib.parse import urlparse, parse_qs
+import requests_cache
+
+# https://github.com/waditu/czsc/blob/master/czsc/fsa/__init__.py
+# const
+
+@retry(stop=stop_after_attempt(3), wait=wait_random(min=1, max=5))
+def request(method, url, headers, payload=None) -> dict:
+    """飞书API标准请求
+
+    :param method: 请求方法
+    :param url: 请求地址
+    :param headers: 请求头
+    :param payload: 传参
+    :return:
+    """
+    payload = {} if not payload else payload
+    response = requests.request(method, url, headers=headers, json=payload)
+    logger.info(f"{'+' * 88}")
+    logger.info(f"URL: {url} || X-Tt-Logid: {response.headers['X-Tt-Logid']}")
+    logger.info(f"headers: {headers}")
+    logger.info(f"payload: {payload}")
+
+    resp = {}
+    if response.text[0] == '{':
+        resp = response.json()
+        logger.info(f"response: {resp}")
+    else:
+        logger.info(f"response: {response.text}")
+
+    code = resp.get("code", -1)
+    if code == -1:
+        code = resp.get("StatusCode", -1)
+    if code == -1 and response.status_code != 200:
+        response.raise_for_status()
+    if code != 0:
+        logger.debug(f"request fail: code={code}, msg={resp.get('msg', '')}")
+        raise ValueError(f"request fail: code={code}, msg={resp.get('msg', '')}")
+    return resp
+
+class UserAccessTokenException(Exception):
+    def __init__(self, message):
+        # Call the base class constructor with the parameters it needs
+        super().__init__(message)
+
+pre_auth_code = None
+# Define a custom request handler to process incoming requests
+class MyHandler(http.server.BaseHTTPRequestHandler):
+    ## https://open.feishu.cn/document/server-docs/authentication-management/login-state-management/obtain-code
+    # https://open.feishu.cn/document/uQjL04CN%2fucDOz4yN4MjL3gzM?code={AuthorizationCode}&state=RANDOMSTATE
+
+    def do_GET(self):
+        global pre_auth_code
+
+        # Parse the query string from the URL
+        parsed_url = urlparse(self.path)
+        query_params = parse_qs(parsed_url.query)
+        # pre_auth_code = request.args.get('code')
+        # trigger_uri = request.args.get('state')
+        # auth.get_user_access_token(pre_auth_code)
+
+
+        # Get user input from the query string
+        pre_auth_code = query_params.get('code')[0]
+        state = query_params.get('state')[0]
+
+        # Process the user input (you can replace this with your logic)
+        response = f"Received access_token, state: {state}"
+
+        # Send the response to the client
+        self.send_response(200)
+        self.send_header('Content-type', 'text/plain')
+        self.end_headers()
+        self.wfile.write(response.encode())
+
+# 开放接口 URI
+TENANT_ACCESS_TOKEN_URI = "/open-apis/auth/v3/tenant_access_token/internal"
+JSAPI_TICKET_URI = "/open-apis/jssdk/ticket/get"
+APP_ACCESS_TOKEN_URI = "/open-apis/auth/v3/app_access_token/internal"
+
+class FeishuAuth(object):
+
+    def __init__(self, feishu_host, app_id, app_secret):
+        self.feishu_host = feishu_host
+        self.app_id = app_id
+        self.app_secret = app_secret
+        self.tenant_access_token = ""
+        self.session = requests_cache.CachedSession(os.path.join(
+            os.getenv('HOME'), '.cache', 'feishu-request-cache'
+        ))
+        try:
+            self.user_access_token_data = json.loads(keyring.get_password("feishu", "user_access_token_data"))
+        except:
+            self.user_access_token_data = None
+
+    def request(self, method, url, params = {}, headers = {}, payload=None) -> dict:
+        """飞书API标准请求
+
+        :param method: 请求方法
+        :param url: 请求地址
+        :param headers: 请求头
+        :param payload: 传参
+        :return:
+        """
+        auth_headers = {
+            "Authorization": "Bearer " + self.user_access_token,
+            'Content-Type' : 'application/json; charset=utf-8'
+        }
+        r_headers = {**auth_headers, **headers}
+        payload = {} if not payload else payload
+        response = self.session.request(method, url, params = params, headers=r_headers, json=payload)
+        logger.info(f"{'+' * 88}")
+        logger.info(f"URL: {url} || X-Tt-Logid: {response.headers['X-Tt-Logid']}")
+        logger.info(f"headers: {r_headers}")
+        logger.info(f"payload: {payload}")
+
+        resp = {}
+        if response.text[0] == '{':
+            resp = response.json()
+            logger.info(f"response: {resp}")
+        else:
+            logger.info(f"response: {response.text}")
+
+        code = resp.get("code", -1)
+        if code == -1:
+            code = resp.get("StatusCode", -1)
+        if code == -1 and response.status_code != 200:
+            response.raise_for_status()
+        if code != 0:
+            logger.debug(f"request fail: code={code}, msg={resp.get('msg', '')}")
+            raise ValueError(f"request fail: code={code}, msg={resp.get('msg', '')}")
+        return resp
+
+    def get_ticket(self):
+        # 获取jsapi_ticket，具体参考文档：https://open.feishu.cn/document/ukTMukTMukTM/uYTM5UjL2ETO14iNxkTN/h5_js_sdk/authorization
+        self.authorize_tenant_access_token()
+        url = "{}{}".format(self.feishu_host, JSAPI_TICKET_URI)
+        headers = {
+            "Authorization": "Bearer " + self.tenant_access_token,
+            "Content-Type": "application/json",
+        }
+        resp = requests.post(url=url, headers=headers)
+        FeishuAuth._check_error_response(resp)
+        return resp.json().get("data").get("ticket", "")
+
+    def get_tenant_access_token(self):
+        if self.tenant_access_token == "":
+            self.authorize_tenant_access_token()
+
+        return self.tenant_access_token
+
+    def authorize_tenant_access_token(self):
+        # 获取tenant_access_token，基于开放平台能力实现，具体参考文档：https://open.feishu.cn/document/ukTMukTMukTM/ukDNz4SO0MjL5QzM/auth-v3/auth/tenant_access_token_internal
+        url = "{}{}".format(self.feishu_host, TENANT_ACCESS_TOKEN_URI)
+        req_body = {"app_id": self.app_id, "app_secret": self.app_secret}
+        response = requests.post(url, req_body)
+        FeishuAuth._check_error_response(response)
+        self.tenant_access_token = response.json().get("tenant_access_token")
+
+    def get_app_access_token(self):
+        url = "{}{}".format(self.feishu_host, APP_ACCESS_TOKEN_URI)
+        req_body = {"app_id": self.app_id, "app_secret": self.app_secret}
+        response = requests.post(url, req_body)
+        FeishuAuth._check_error_response(response)
+        logger.debug(response.json())
+        self.app_access_token = response.json().get("app_access_token")
+        return self.app_access_token
+
+    def get_user_access_token_url(self, trigger_uri = '/'):
+        params = urllib.parse.urlencode({'app_id': self.app_id, 'redirect_uri': 'http://localhost:3000/user_appcess_token', 'state': trigger_uri})
+        url = "{}{}?{}".format(self.feishu_host, "/open-apis/authen/v1/index", params)
+        return url
+
+    @property
+    def user_access_token(self):
+        global pre_auth_code
+        try:
+            return self.get_user_access_token()
+        except UserAccessTokenException as e:
+            import socketserver
+            pre_auth_code = None
+            target_url = self.get_user_access_token_url()
+            os.system("open '{:s}'".format(target_url))
+            with socketserver.TCPServer(("", 3000), MyHandler) as httpd:
+                httpd.handle_request()
+
+            if pre_auth_code is not None:
+                return self.get_user_access_token(pre_auth_code)
+
+    def get_user_access_token(self, code = None):
+        '''
+{
+    "code": 0,
+    "msg": "success",
+    "data": {
+        "access_token": "u-Q7JWnaIM_kRChuLfreHmpArjOEayt.5XUBJcZr.V0Gst4FdQCtvrd9sAViLXQnQgkpL19brGOjKZQTxb",
+        "token_type": "Bearer",
+        "expires_in": 7140,
+        "name": "zhangsan",
+        "en_name": "Three Zhang",
+        "avatar_url": "www.feishu.cn/avatar/icon",
+        "avatar_thumb": "www.feishu.cn/avatar/icon_thumb",
+        "avatar_middle": "www.feishu.cn/avatar/icon_middle",
+        "avatar_big": "www.feishu.cn/avatar/icon_big",
+        "open_id": "ou_caecc734c2e3328a62489fe0648c4b98779515d3",
+        "union_id": "on_d89jhsdhjsajkda7828enjdj328ydhhw3u43yjhdj",
+        "email": "zhangsan@feishu.cn",
+        "enterprise_email": "demo@mail.com",
+        "user_id": "5d9bdxxx",
+        "mobile": "+86130002883xx",
+        "tenant_key": "736588c92lxf175d",
+        "refresh_expires_in": 2591940,
+        "refresh_token": "ur-oQ0mMq6MCcueAv0pwx2fQQhxqv__CbLu6G8ySFwafeKww2Def2BJdOkW3.9gCFM.LBQgFri901QaqeuL",
+        "sid": "AAAAAAAAAANjgHsqKEAAEw=="
+    }
+}
+        '''
+        if self.user_access_token_data is not None:
+            if time.time() <= self.user_access_token_data.get('expires_at'):
+                return self.user_access_token_data.get('access_token')
+
+        if code is None:
+            raise UserAccessTokenException('No valid user_access_token')
+
+        app_access_token = self.get_app_access_token();
+        logger.debug("app_access_token: {}".format(app_access_token))
+        url = "{}{}".format(self.feishu_host, '/open-apis/authen/v1/access_token')
+        payload = json.dumps({
+	    "code": code,
+	    "grant_type": "authorization_code"
+        })
+        headers = {
+            'Content-Type': 'application/json',
+            'Authorization': 'Bearer ' + app_access_token
+        }
+        response = requests.request("POST", url, headers=headers, data=payload)
+        FeishuAuth._check_error_response(response)
+
+        self.user_access_token_data = response.json().get('data')
+        self.user_access_token_data.update({'expires_at' : time.time() + self.user_access_token_data.get('expires_in') - 5})
+        keyring.set_password("feishu", "user_access_token_data", json.dumps(self.user_access_token_data))
+
+        return self.user_access_token_data.get('access_token')
+        # self.app_access_token = response.json().get("tenant_access_token")
+
+
+    @staticmethod
+    def _check_error_response(resp):
+        # 检查响应体是否包含错误信息
+        if resp.status_code != 200:
+            raise resp.raise_for_status()
+        response_dict = resp.json()
+        code = response_dict.get("code", -1)
+        if code != 0:
+            logging.error(response_dict)
+            raise FeishuException(code=code, msg=response_dict.get("msg"))
+
+
+class FeishuException(Exception):
+    # 处理并展示飞书侧返回的错误码和错误信息
+    def __init__(self, code=0, msg=None):
+        self.code = code
+        self.msg = msg
+
+    def __str__(self) -> str:
+        return "{}:{}".format(self.code, self.msg)
+
+    __repr__ = __str__
---
+++ get-feishu-table.py
@@ -0,0 +1,265 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+#   get-feishu-table.py --- Description
+#
+#   Copyright (C) 2023, Schspa, all rights reserved.
+#
+#   This program is free software; you can redistribute it and/or modify
+#   it under the terms of the GNU General Public License as published by
+#   the Free Software Foundation; either version 2 of the License, or
+#   (at your option) any later version.
+#
+#   This program is distributed in the hope that it will be useful,
+#   but WITHOUT ANY WARRANTY; without even the implied warranty of
+#   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#   GNU General Public License for more details.
+#
+#   You should have received a copy of the GNU General Public License
+#   along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+
+import os
+import sys
+import time
+import hashlib
+import requests
+import keyring
+import json
+from loguru import logger
+import http.server
+import socketserver
+from urllib.parse import urlparse, parse_qs
+from auth import FeishuAuth
+import threading
+import click
+
+def column_number_to_name(column_number):
+    if column_number <= 0:
+        raise ValueError("Column number should be greater than 0")
+
+    column_name = ""
+    while column_number > 0:
+        remainder = (column_number - 1) % 26  # 0 to 25 for A to Z
+        column_name = chr(65 + remainder) + column_name
+        column_number = (column_number - 1) // 26  # Integer division for next digit
+
+    return column_name
+
+def get_cell_name(row, col):
+    return '{:s}{:d}'.format(column_number_to_name(col), row)
+
+class SpreadSheets(FeishuAuth):
+    """
+    电子表格概述: https://open.feishu.cn/document/ukTMukTMukTM/uATMzUjLwEzM14CMxMTN/overview
+    """
+
+    def __init__(self, feishu_host, app_id, app_secret, url):
+        super().__init__(feishu_host, app_id, app_secret)
+        self.host = feishu_host
+        self.url = url
+        parsed_url = urlparse(url)
+        url_path = parsed_url.path
+        query_dict = parse_qs(parsed_url.query)
+        # Split the URL path into parts
+        path_parts = url_path.split('/')
+        # Remove empty parts
+        path_parts = [part for part in path_parts if part]
+        self.sheet_info = {}
+
+        if path_parts[0] == 'wiki':
+            # https://open.feishu.cn/wiki/xxxxxxxxxxxx?sheet=xxx
+            logger.info("Get a wiki file, try to get it's node info")
+            node_info = super().request('GET', 'https://open.feishu.cn/open-apis/wiki/v2/spaces/get_node', params = {
+                'obj_type': 'wiki',
+                'token': path_parts[1]
+            })['data']
+            obj_token = node_info['node']['obj_token']
+            obj_type = node_info['node']['obj_type']
+            if obj_type != 'sheet':
+                raise Exception
+            title = node_info['node']['title']
+            logger.debug(f'obj_type: { obj_type }, obj_token: { obj_token } title: { title }')
+            self.sheet_token = obj_token
+        else:
+            self.sheet_token = path_parts[1]
+
+        sheet_id = query_dict.get('sheet', None)
+        if sheet_id is not None:
+            self.sheet_id = sheet_id[0]
+        else:
+            self.sheet_id = None
+
+        logger.debug(f'Create sheet object token: {self.sheet_token}, sheet_id: {self.sheet_id}')
+
+
+    def check(self):
+        """获取电子表格信息
+
+        https://open.feishu.cn/document/ukTMukTMukTM/uUDN04SN0QjL1QDN/sheets-v3/spreadsheet/get
+
+        :param token: 表格的token，示例值："shtxxxxxxxxxxxxxxx"
+        :return: 返回数据样例如下
+                {
+                    "code": 0,
+                    "msg": "success",
+                    "data": {
+                        "spreadsheet": {
+                            "title": "title",
+                            "owner_id": "ou_xxxxxxxxxxxx",
+                            "token": "shtxxxxxxxxxxxxxx",
+                            "url": "https://bytedance.feishu.cn/sheets/shtcnmBA*****yGehy8"
+                        }
+                    }
+                }
+        """
+        url = f"{self.host}/open-apis/sheets/v3/spreadsheets/{self.sheet_token}"
+        return super().request('GET', url)
+
+    @property
+    def sheets(self):
+        sheets = []
+        url = 'https://open.feishu.cn/open-apis/sheets/v3/spreadsheets/{:s}/sheets/query'.format(self.sheet_token)
+        sheet_tables = super().request("GET", url)
+        return sheet_tables['data']['sheets']
+
+    def get_sheet_info(self, sheet_id = None):
+        if sheet_id is None:
+            sheet_id = self.sheet_id
+        sheet_info = self.sheet_info.get(sheet_id, None)
+        if sheet_info is None:
+            ranges = '{:s}!A:AM'.format(self.sheet_id)
+            url = 'https://open.feishu.cn/open-apis/sheets/v3/spreadsheets/{:s}/sheets/{:s}'.format(self.sheet_token, sheet_id)
+            sheet_info = super().request("GET", url)
+            self.sheet_info[sheet_id] = sheet_info
+
+        return sheet_info
+
+    def get_max_row(self, sheet_id = None):
+        '''
+        {'code': 0, 'data': {'sheet': {'grid_properties': {'column_count': 10, 'frozen_column_count': 0, 'frozen_row_count': 1, 'row_count': 741}, 'hidden': False, 'index': 1, 'merges':
+        [
+        {'end_column_index': 0, 'end_row_index': 316, 'start_column_index': 0, 'start_row_index': 291},
+        {'end_column_index': 0, 'end_row_index': 393, 'start_column_index': 0, 'start_row_index': 375},
+        {'end_column_index': 0, 'end_row_index': 374, 'start_column_index': 0, 'start_row_index': 344}, {'end_column_index': 0, 'end_row_index': 343, 'start_column_index': 0, 'start_row_index': 317}, {'end_column_index': 0, 'end_row_index': 290, 'start_column_index': 0, 'start_row_index': 269}, {'end_column_index': 0, 'end_row_index': 268, 'start_column_index': 0, 'start_row_index': 251}, {'end_column_index': 0, 'end_row_index': 250, 'start_column_index': 0, 'start_row_index': 207}, {'end_column_index': 0, 'end_row_index': 153, 'start_column_index': 0, 'start_row_index': 136}, {'end_column_index': 0, 'end_row_index': 135, 'start_column_index': 0, 'start_row_index': 130}, {'end_column_index': 0, 'end_row_index': 129, 'start_column_index': 0, 'start_row_index': 118}, {'end_column_index': 0, 'end_row_index': 117, 'start_column_index': 0, 'start_row_index': 106}, {'end_column_index': 0, 'end_row_index': 105, 'start_column_index': 0, 'start_row_index': 95}, {'end_column_index': 0, 'end_row_index': 94, 'start_column_index': 0, 'start_row_index': 85}, {'end_column_index': 0, 'end_row_index': 84, 'start_column_index': 0, 'start_row_index': 64}, {'end_column_index': 0, 'end_row_index': 63, 'start_column_index': 0, 'start_row_index': 43}, {'end_column_index': 0, 'end_row_index': 42, 'start_column_index': 0, 'start_row_index': 22}, {'end_column_index': 0, 'end_row_index': 21, 'start_column_index': 0, 'start_row_index': 1}, {'end_column_index': 0, 'end_row_index': 182, 'start_column_index': 0, 'start_row_index': 154}, {'end_column_index': 0, 'end_row_index': 206, 'start_column_index': 0, 'start_row_index': 183}, {'end_column_index': 0, 'end_row_index': 458, 'start_column_index': 0, 'start_row_index': 454}, {'end_column_index': 0, 'end_row_index': 473, 'start_column_index': 0, 'start_row_index': 459}, {'end_column_index': 0, 'end_row_index': 453, 'start_column_index': 0, 'start_row_index': 449}, {'end_column_index': 0, 'end_row_index': 448, 'start_column_index': 0, 'start_row_index': 394}, {'end_column_index': 0, 'end_row_index': 596, 'start_column_index': 0, 'start_row_index': 524}, {'end_column_index': 0, 'end_row_index': 478, 'start_column_index': 0, 'start_row_index': 474}, {'end_column_index': 0, 'end_row_index': 522, 'start_column_index': 0, 'start_row_index': 479}, {'end_column_index': 0, 'end_row_index': 613, 'start_column_index': 0, 'start_row_index': 597}, {'end_column_index': 0, 'end_row_index': 632, 'start_column_index': 0, 'start_row_index': 615}, {'end_column_index': 0, 'end_row_index': 643, 'start_column_index': 0, 'start_row_index': 640}, {'end_column_index': 0, 'end_row_index': 639, 'start_column_index': 0, 'start_row_index': 633}, {'end_column_index': 0, 'end_row_index': 661, 'start_column_index': 0, 'start_row_index': 656}, {'end_column_index': 0, 'end_row_index': 672, 'start_column_index': 0, 'start_row_index': 662}, {'end_column_index': 0, 'end_row_index': 692, 'start_column_index': 0, 'start_row_index': 673}, {'end_column_index': 0, 'end_row_index': 696, 'start_column_index': 0, 'start_row_index': 693}, {'end_column_index': 0, 'end_row_index': 701, 'start_column_index': 0, 'start_row_index': 698}, {'end_column_index': 0, 'end_row_index': 712, 'start_column_index': 0, 'start_row_index': 702},
+        {'end_column_index': 0, 'end_row_index': 734, 'start_column_index': 0, 'start_row_index': 713},
+        {'end_column_index': 0, 'end_row_index': 655, 'start_column_index': 0, 'start_row_index': 644}
+        ], 'resource_type': 'sheet', 'sheet_id': 'I1Z4dW', 'title': 'aaa'}}, 'msg': ''}
+        '''
+        sheet_info = self.get_sheet_info()
+        grid_properties = sheet_info['data']['sheet']['grid_properties']
+        return grid_properties['row_count'], grid_properties['column_count']
+
+    def get_merge_info(self, sheet_id = None):
+        sheet_info = self.get_sheet_info()
+        if 'merges' in sheet_info['data']['sheet'].keys():
+            return sheet_info['data']['sheet']['merges']
+        else:
+            return []
+
+    def get_value(self, row, column):
+        ranges = '{sheet_id:s}!{column_name:s}{row:d}:{column_name:s}{row:d}'.format(
+            sheet_id = self.sheet_id,
+            column_name = column_number_to_name(column),
+            row = row
+        )
+        url = 'https://open.feishu.cn/open-apis/sheets/v2/spreadsheets/{:s}/values/{:s}'.format(self.sheet_token, ranges)
+        sheet_tables = super().request("GET", url)
+        return sheet_tables['data']
+
+
+    def get_table_data(self):
+        max_row, max_column = self.get_max_row()
+        column_idx = column_number_to_name(max_column)
+        logger.debug(f'max_row: {max_row} max_column: {max_column}, column_idx: {column_idx}')
+        ranges = '{:s}!A:{:s}'.format(self.sheet_id, column_idx)
+        url = 'https://open.feishu.cn/open-apis/sheets/v2/spreadsheets/{:s}/values/{:s}'.format(self.sheet_token, ranges)
+        sheet_tables = super().request("GET", url)
+        return sheet_tables['data']
+
+    def to_excel(self, file_path):
+        from openpyxl import Workbook
+        wb = Workbook()
+        for sheet in self.sheets:
+            logger.debug(f"Process sheet: {sheet['title']} id: { sheet['sheet_id'] }")
+            self.sheet_id = sheet['sheet_id']
+            ws = wb.create_sheet(sheet['title'], -1)
+
+            table_data = self.get_table_data()
+
+            row = 0
+            col = 0
+            for rv in table_data['valueRange']['values']:
+                row = row + 1
+                col = 0
+                for v in rv:
+                    col = col + 1
+                    cell = ws.cell(row = row, column = col)
+                    try:
+                        cell.value = v
+                    except ValueError:
+                        if isinstance(v, dict):
+                            cell.value = v['text']
+                        if isinstance(v, list):
+                            text = ''
+                            for line in v:
+                                text = text + line['text']
+
+                            cell.value = text
+
+            # do merge cell
+            for merge_info in self.get_merge_info():
+                # {'end_column_index': 0, 'end_row_index': 316, 'start_column_index': 0, 'start_row_index': 291},
+                start = get_cell_name(row = merge_info['start_row_index'] + 1, col = merge_info['start_column_index'] + 1)
+                end = get_cell_name(row = merge_info['end_row_index'] + 1, col = merge_info['end_column_index'] + 1)
+                merge_range = '{:s}:{:s}'.format(start, end)
+                logger.debug(f"Merge {merge_range}")
+                ws.merge_cells(merge_range)
+
+        wb.save(file_path)
+
+FEISHU_HOST = 'https://open.feishu.cn'
+
+@click.command()
+@click.option('-u', '--sheet-url', 'sheet_url', type = str, default = None)
+@click.option('-o', '--output', 'output_file', type = click.File('wb'), default = None)
+@click.option('--app_cfg', 'app_cfg', type = str, default = 'default')
+@click.option('--setup', 'setup', is_flag = True, default = False)
+@click.option('-v', '--verbose', is_flag=True, default = False)
+def get_feishu_table(sheet_url, output_file, app_cfg, setup, verbose):
+    handler = {"sink": sys.stderr, "level": "ERROR"}
+    if verbose:
+        handler = {"sink": sys.stderr, "level": "DEBUG"}
+    logger.configure(handlers=[handler])
+
+    feishu_cfg = keyring.get_password('FeishuApp', app_cfg)
+    app_id = None
+    app_secret = None
+    feishu_host = 'https://open.feishu.cn'
+    if feishu_cfg is None or setup:
+        import getpass
+
+        app_id = getpass.getpass(f'Please input the feishu app id:')
+        app_secret = getpass.getpass(f'Please input the feishu app secret:')
+        feishu_host = getpass.getpass(f'Please input the feishu host url, default: {feishu_host}:')
+        if feishu_host == '':
+            feishu_host = FEISHU_HOST
+        feishu_cfg = {
+            'app_id' : app_id,
+            'app_secret': app_secret,
+            'feishu_host': feishu_host,
+        }
+        keyring.set_password('FeishuApp', app_cfg, json.dumps(feishu_cfg))
+        logger.success(f'Feishu App Config for {app_cfg} saved to keyring')
+        pass
+    else:
+        feishu_cfg = json.loads(feishu_cfg)
+
+    sheets = SpreadSheets(
+        feishu_cfg['feishu_host'], feishu_cfg['app_id'],
+        feishu_cfg['app_secret'], sheet_url)
+    sheets.check()
+    sheets.to_excel(output_file)
+
+if __name__ == '__main__':
+    get_feishu_table()
