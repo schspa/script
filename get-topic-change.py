c02910914df162608c4e3eb09e155b986bbbf36c
Schspa Shi <schspa@gmail.com>
2024-04-26 21:23:45 +0800

    gerrit-topic: add correct patches sequence support
    Signed-off-by: Schspa Shi <schspa@gmail.com>

--- get-topic-change.py
+++ get-topic-change.py
@@ -18,6 +18,7 @@
 #   You should have received a copy of the GNU General Public License
 #   along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+import os
 import json
 import time
 import getpass
@@ -27,19 +28,100 @@ import requests
 import subprocess
 import tempfile
 import shlex
+from urllib.parse import urlparse
+from pprint import pprint
 
+
+def arrange_patchs(patches):
+    for project, pi in patches.items():
+        click.secho('arrange for %s' % (project), bg='black', fg='white')
+        out = []
+        found = True
+        done = False
+
+        patchs = pi['patches']
+        while not done:
+            found = False
+            for i in range(len(patchs)):
+                if len(out) == 0:
+                    out.append(patchs[i])
+                    patchs.pop(i)
+                    found = True
+                    break;
+                for j in range(len(out)):
+                    pprint(out[j])
+                    pprint(patchs[i])
+                    if out[j]['rev'] == patchs[i]['parent']:
+                        out.insert(j + 1, patchs[i])
+                        patchs.pop(i)
+                        found = True
+                        break
+                    if out[j]['parent'] == patchs[i]['rev']:
+                        out.insert(j, patchs[i])
+                        patchs.pop(i)
+                        found = True
+                        break
+
+            if not found:
+                done = True
+                break
+
+        pi['patches'] = out
+
+def get_directory_path_from_url(url):
+    parsed_url = urlparse(url)
+    path_segments = parsed_url.path.split('/')[1:]  # Split path and remove the leading empty segment
+
+    # Assuming the structure is always /c/<directory_path>/+/<numbers>
+    # and you want to retrieve only the <directory_path> part:
+    if len(path_segments) > 1 and path_segments[0] == 'c':
+        directory_path = '/'.join(path_segments[1:-2])  # Exclude the '+/numbers' segment
+        return directory_path
+    return None  # Return None or appropriate value if URL doesn't match the expected pattern
+
+# url = "https://gerrit.xxx.cc:8443/c/cs/aa/bb/+/15198"
+# print(get_directory_path_from_url(url))
+
+def repo_info_to_dict(project):
+    # Execute the 'repo info' command and capture the output
+    result = subprocess.run(['repo', 'info', project], stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
+
+    # Check if the command was successful
+    if result.returncode != 0:
+        print("Error running 'repo info':", result.stderr)
+        return None
+
+    # Parse the output to create a dictionary
+    repo_info_dict = {}
+    for line in result.stdout.split('\n'):
+        # Split each line into a key-value pair
+        parts = line.split(': ', 1)
+        if len(parts) == 2:
+            key, value = parts
+            repo_info_dict[key.strip()] = value.strip()
+
+    return repo_info_dict
 
 @click.command()
 @click.option('-r', '--url', default='gerrit.test.com')
-@click.option('-p', '--port', type=int, default=8443)
+@click.option('-p', '--port', type=int, default=29418)
 @click.option('-u', '--user', default='admin')
 @click.option('-t', '--topic', default='topic')
 @click.option('-v', '--verbose', is_flag=True)
-def get_gerrit_changes(url, port, user, topic, verbose):
+@click.option('-n', '--not_apply', is_flag=True, default=False)
+def get_gerrit_changes(url, port, user, topic, verbose, not_apply):
     """Create a new gitlab build and wait for build exit with status"""
     commands = shlex.split('ssh -p {:d} {:s}@{:s} gerrit query --format=JSON --current-patch-set status:open topic:{:s}'.format(port, user, url, topic))
     output = subprocess.check_output(
         commands, stderr=subprocess.STDOUT).decode("utf-8")
+    with open(os.path.join(os.getcwd(), 'gerrit-query-out.txt'), 'w') as f:
+        f.write(output)
+
+    # output = ''
+    # with open(os.path.join(os.path.expanduser('~/gerrit-query-out.txt')), 'r') as f:
+    #     output = f.read()
+
+    patchs = {}
     for item in output.splitlines():
         res = json.loads(item)
         if verbose:
@@ -48,26 +130,73 @@ def get_gerrit_changes(url, port, user, topic, verbose):
         if 'type' in res.keys():
             continue
 
+        pprint(res)
         project = res['project']
         subject = res['subject']
+        ref = res['currentPatchSet']['ref']
+        rev = res['currentPatchSet']['revision']
+        parent = res['currentPatchSet']['parents'][0]
+        current_patch = {
+            'subject': subject,
+            'ref': ref,
+            'rev': rev,
+            'parent': parent,
+            'project': parent,
+            'next': None
+        }
+        if project not in patchs.keys():
+            # first patchset in current project
+            patchs[project] = {}
+            patchs[project]['patches'] = [current_patch]
+        else:
+            patchs[project]['patches'].append(current_patch)
+
+        patchs[project]['branch'] = res['branch']
+        patchs[project]['url'] = res['url']
+        patchs[project]['rpath'] = get_directory_path_from_url(res['url'])
 
         project_dir = project.replace('cs/self/', '')
-        ref = res['currentPatchSet']['ref']
         click.secho('Project: %s' % (project), bg='black', fg='white')
         click.secho('Subject: %s' % (subject), bg='black', fg='white')
         click.secho('Ref: %s' % (ref), bg='black', fg='white')
-        commands = shlex.split('git fetch ssh://{:s}@{:s}:{:d}/{:s} {:s}'.format(user, url, port, project, ref))
-        output = subprocess.check_output(
-            commands,
-            cwd=project_dir,
+        click.secho('Parent: %s' % (parent), bg='black', fg='white')
+        click.secho('Revision: %s' % (rev), bg='black', fg='white')
+        click.secho('---------------------------------', bg='black', fg='white')
+        if not_apply:
+            continue
+
+    arrange_patchs(patchs)
+    pprint(patchs)
+
+    project_paths = subprocess.check_output(
+        shlex.split('repo list -p'), stderr=subprocess.STDOUT).decode("utf-8")
+    for project in project_paths.splitlines():
+        click.secho('project: %s' % (project), bg='black', fg='white')
+        ri = repo_info_to_dict(project)
+        if ri is None:
+            continue
+        # pprint(ri)
+        p_mnt = ri['Mount path']
+        repoinfo = subprocess.check_output(
+            shlex.split('git config -l'.format(project)),
+            cwd=p_mnt,
             stderr=subprocess.STDOUT).decode("utf-8")
-        print(output)
-        commands = shlex.split('git reset --hard FETCH_HEAD')
-        output = subprocess.check_output(
-            commands,
-            cwd=project_dir,
-            stderr=subprocess.STDOUT).decode("utf-8")
-        print(output)
+        for project, pi in patchs.items():
+            if pi['rpath'] not in repoinfo:
+                continue
+            for patch in pi['patches']:
+                ref = patch['ref']
+                click.secho('applying patch: %s' % (patch['subject']), bg='black', fg='white')
+                commands = shlex.split('git fetch ssh://{:s}@{:s}:{:d}/{:s} {:s}'.format(user, url, port, project, ref))
+                output = subprocess.check_output(
+                    commands,
+                    cwd=p_mnt,
+                    stderr=subprocess.STDOUT).decode("utf-8")
+                commands = shlex.split('git cherry-pick FETCH_HEAD')
+                output = subprocess.check_output(
+                    commands,
+                    cwd=p_mnt,
+                    stderr=subprocess.STDOUT).decode("utf-8")
 
     sys.exit(0)
 
